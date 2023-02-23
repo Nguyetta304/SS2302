0335a817c6043258af442a3e612a8ea61743e1c5
Nguyetta304 <tanguyet3004@gmail.com>
2023-02-23 22:37:18 +0700

    Update Ex6.java

--- Ex6.java
+++ Ex6.java
@@ -9,10 +9,14 @@ public class Ex6 {
         double a = in.nextDouble();
         double b = in.nextDouble();
         double c = in.nextDouble();
+        double cv = a+b+c;
+        double p = cv/2;
+        double dt = Math.sqrt(p*(p-a)*(p-b)*(p-c));
                 if ((a < b+c) || (b < a+c ) || (c< a+b))
                     System.out.println(" abc la tam giac");
                 else
                     System.out.println(" abc khong phai la tam giac");
+        System.out.printf( "chu vi cua tam giac : %10.2f cm, dien tich cua tam giac : %10.2f cm2",cv,dt);
 
     }
 }
